27438a58f3919a3f26992b4285b789004bfccd57
SwG Ghosh <codecrafts.cf@icloud.com>
2016-09-30 20:18:55 +0530

    Flock incoming hook post SSL verify set to false, to fix flock group posting

--- flockincominghook.php
+++ flockincominghook.php
@@ -1,13 +1,21 @@
 <?php
 function flock_group_post($string) {
-  $url = 'https://api.flock.co/hooks/sendMessage/67ecab90-6b4d-46fa-b58e-b60ff9c4ff48';
-  $options = array(
+    $data = array('text' => $string);
+    $url = 'https://api.flock.co/hooks/sendMessage/67ecab90-6b4d-46fa-b58e-b60ff9c4ff48';
+    $options = array(
         'http' => array(
-            'header'  => "Content-type: application/json\r\n",
-            'method'  => 'POST',
-            'content' => "{\"text\": \"".$string."\"}",
+        'method'  => 'POST',
+        'content' => json_encode( $data ),
+        'header'=>  "Content-Type: application/json\r\n" .
+                    "Accept: application/json\r\n"
+        ),
+        "ssl"=>array(
+            "verify_peer"=>false,
+            "verify_peer_name"=>false,
         )
-  );
-  $context  = stream_context_create($options);
-  $result = file_get_contents($url, false, $context);
+    );
+    
+    $context  = stream_context_create( $options );
+    $result = file_get_contents( $url, false, $context );
+    $response = json_decode( $result );
 }
